7b838b1b0983b7cc84f05820bb54405e853ec0bf
AlexRuiz7687 <alexisruiz.daw@gmail.com>
2025-10-28 09:11:22 +0000

    Primer commit

---
+++ js/menu.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navList = document.querySelector('.navbar ul');
+    navList.classList.toggle('active');
+});
