006ee6ebb33b94c95be9953d65f45b78f3b08c68
AlexRuiz7687 <alexisruiz.daw@gmail.com>
2025-12-03 08:31:19 +0000

    Cambios en login: Local Storage

--- assets/js/menu.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const navList = document.querySelector('.navbar ul');
-    navList.classList.toggle('active');
-});
